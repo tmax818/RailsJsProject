435bd2b6e2a95d10593d7e3882d22e8a8d764314
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-10-03 14:02:23 -0700

    display index with json

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,12 +1,30 @@
-class BookByTitle {
-  constructor(id, title, firstName, lastName) {
-    this.id = id
-    this.title = title,
-    this.firstName = firstName,
-    this.lastName = lastName
+class Book {
+  constructor(title, author_first, author_last) {
+    this.title = title;
+    this.author_first = author_first;
+    this.author_last = author_last;
   }
 
   fetchBooks() {
-    fetch("http://localhost:3000/books/by_title.json").then(res => res.json()).then(res => console.log(res))
+    fetch('http://localhost:3000/books/by_title.json')
+      .then(res => res.json())
+      .then(json => this.createBookList(json));
+  }
+
+  createBookList(data) {
+    var books = [];
+    data.forEach(d =>
+      books.push(new Book(d.title, d.author_first, d.author_last))
+    );
+    books.forEach(book => {
+      document.getElementById('books').innerHTML += `
+      <ul>
+        <li>${book.title} By:${book.author_first} ${book.author_last} </li>
+        </ul>
+      `;
+    });
   }
 }
+
+let book = new Book();
+book.fetchBooks();
