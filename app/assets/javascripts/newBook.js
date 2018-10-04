85787c74733a1e775286206a2288254a51f56e7d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-10-04 13:36:10 -0700

    finish NewBook class and connect form from new.html.erb

--- app/assets/javascripts/newBook.js
+++ app/assets/javascripts/newBook.js
@@ -5,3 +5,17 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   console.log(input);
 });
+
+class NewBook {
+  constructor(title, author_first, author_last, review) {
+    this.title = title;
+    this.author_first = author_first;
+    this.author_last = author_last;
+    this.review = review;
+  }
+
+  postNewBook() {}
+}
+
+let book = new NewBook();
+console.log(book);
