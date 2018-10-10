8d38f1a80c1d969cb942ccac072d81d43b41ecd8
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-10-10 13:42:24 -0700

    Add links to book title index page.

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,12 +1,13 @@
 class Book {
-  constructor(title, author_first, author_last) {
+  constructor(id, title, author_first, author_last) {
+    this.id = id;
     this.title = title;
     this.author_first = author_first;
     this.author_last = author_last;
   }
 
   fetchBooks() {
-    fetch('http://localhost:3000/books/by_title.json')
+    fetch("http://localhost:3000/books/by_title.json")
       .then(res => res.json())
       .then(json => this.createBookList(json));
   }
@@ -14,12 +15,14 @@ class Book {
   createBookList(data) {
     var books = [];
     data.forEach(d =>
-      books.push(new Book(d.title, d.author_first, d.author_last))
+      books.push(new Book(d.id, d.title, d.author_first, d.author_last))
     );
     books.forEach(book => {
-      document.getElementById('books').innerHTML += `
+      document.getElementById("books").innerHTML += `
       <ul>
-        <li>${book.title} By:${book.author_first} ${book.author_last} </li>
+        <li><a href="/books/${book.id}">${book.title}</a> By:${
+        book.author_first
+      } ${book.author_last} </li>
         </ul>
       `;
     });
--- app/assets/javascripts/newBook.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("new").innerHTML = "hi";
